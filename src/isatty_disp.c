d79c6ca1f91294c1841fe807cff5753c4dd54a23
Un-Velo6Raptor <martin.januario@epitech.eu>
2017-04-28 15:11:00 +0200

    prompt color disabled

--- src/isatty_disp.c
+++ src/isatty_disp.c
@@ -5,7 +5,7 @@
 ** Login   <martin.januario@epitech.eu>
 ** 
 ** Started on  Sun Apr  9 02:46:54 2017 Martin Januario
-** Last update Thu Apr 27 15:49:38 2017 Martin Januario
+** Last update Fri Apr 28 15:10:28 2017 Martin Januario
 */
 
 #include	<stdlib.h>
@@ -16,9 +16,7 @@ void		isatty_disp(t_needs *news)
 {
   if (isatty(0) == 1)
     {
-      my_putstr("\033[31m");
       my_putstr(news->pwd);
       my_putstr(" --> ");
-      my_putstr("\033[00m");
     }
 }
